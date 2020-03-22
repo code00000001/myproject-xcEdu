250eba95a4d0f6336590cafcc223698c08c65811
code00000001 <1976331463@qq.com>
2020-03-22 11:43:42 +0800

    【day06】页面发布-消费方，bug修复

--- xc-service-cms-manage-client/src/main/java/com/xuecheng/cms_manage_client/ManageCmsApplication.java
+++ xc-service-cms-manage-client/src/main/java/com/xuecheng/cms_manage_client/ManageCmsApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms") //扫描实体类
-@ComponentScan("com.xuecheng.framework") //扫描common下的所有类
-@ComponentScan("com.xuecheng.cms_manage_client") //扫描本包下的所有类
+@ComponentScan(basePackages = {"com.xuecheng.framework"}) //扫描common下的所有类
+@ComponentScan(basePackages = {"com.xuecheng.cms_manage_client"}) //扫描本包下的所有类
 public class ManageCmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageCmsApplication.class, args);
--- xc-service-cms-manage-client/src/main/java/com/xuecheng/cms_manage_client/mq/ConsumerPostPage.java
+++ xc-service-cms-manage-client/src/main/java/com/xuecheng/cms_manage_client/mq/ConsumerPostPage.java
@@ -1,11 +1,14 @@
-package com.xuecheng.cms_manage_client.service;
+package com.xuecheng.cms_manage_client.mq;
 
 import com.alibaba.fastjson.JSON;
 import com.xuecheng.cms_manage_client.dao.CmsPageRepository;
+import com.xuecheng.cms_manage_client.service.PageService;
 import com.xuecheng.framework.domain.cms.CmsPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -21,6 +24,7 @@ public class ConsumerPostPage {
     private CmsPageRepository cmsPageRepository;
     @Autowired
     private PageService pageService;
+    @RabbitListener(queues = {"${xuecheng.mq.queue}"})
     public void postPage(String msg){
         //解析json消息
         Map map = JSON.parseObject(msg, Map.class);
